e0777f2820bd0f082dcf92fabe8c1a140afcc8e7
zhouming <minerchow@126.com>
2025-10-19 15:25:46 +0800

    update

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,3 +1,31 @@
+import { useEffect } from "react";
+import { useComponetsStore } from "../stores/components";
 export function EditArea() {
-    return <div>EditArea</div>
+    const {components, addComponent} = useComponetsStore();
+    // const {components, deleteComponent} = useComponetsStore();
+    useEffect(()=> {
+        addComponent({
+            id: 222,
+            name: 'Container',
+            props: {},
+            children: []
+        }, 1);
+        addComponent({
+            id: 333,
+            name: 'Video',
+            props: {},
+            children: []
+        }, 222);
+        // setTimeout(() => {
+        //     deleteComponent(333);
+        // }, 3000);
+    }, []);
+
+    return <div>
+        <pre>
+            {
+                JSON.stringify(components, null, 2)
+            }
+        </pre>
+    </div>
 }
