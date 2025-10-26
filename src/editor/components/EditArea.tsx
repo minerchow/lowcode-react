2ca6cedeef2996ab264f9e46ea8d181b1fd4277e
zhouming <minerchow@126.com>
2025-10-26 09:57:02 +0800

    update

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,31 +1,68 @@
-import { useEffect } from "react";
-import { useComponetsStore } from "../stores/components";
-export function EditArea() {
-    const {components, addComponent} = useComponetsStore();
-    // const {components, deleteComponent} = useComponetsStore();
-    useEffect(()=> {
-        addComponent({
-            id: 222,
-            name: 'Container',
-            props: {},
-            children: []
-        }, 1);
-        addComponent({
-            id: 333,
-            name: 'Video',
-            props: {},
-            children: []
-        }, 222);
-        // setTimeout(() => {
-        //     deleteComponent(333);
-        // }, 3000);
-    }, []);
+import React, { MouseEventHandler, useEffect, useState } from "react";
+import { useComponentConfigStore } from "../stores/component-config";
+import { Component, useComponetsStore } from "../stores/components"
 
-    return <div>
-        <pre>
-            {
-                JSON.stringify(components, null, 2)
+export function EditArea() {
+    const { components, addComponent } = useComponetsStore();
+    const { componentConfig } = useComponentConfigStore();
+    const [hoverComponentId, setHoverComponentId] = useState<number>();
+    const handleMouseOver: MouseEventHandler = (e)  => {
+    const path = e.nativeEvent.composedPath();
+        for (let i = 0; i < path.length; i += 1) {
+            const ele = path[i] as HTMLElement;
+
+            const componentId = ele.dataset?.componentId;
+            if (componentId) {
+                setHoverComponentId(+componentId);
+                return;
             }
-        </pre>
+        }
+    }
+    // useEffect(()=> {
+    //     addComponent({
+    //         id: 222,
+    //         name: 'Container',
+    //         props: {},
+    //         children: []
+    //     }, 1);
+
+    //     addComponent({
+    //         id: 333,
+    //         name: 'Button',
+    //         props: {
+    //             text: '无敌'
+    //         },
+    //         children: []
+    //     }, 222);
+    // }, []);
+
+    function renderComponents(components: Component[]): React.ReactNode {
+        return components.map((component: Component) => {
+            const config = componentConfig?.[component.name]
+
+            if (!config?.component) {
+                return null;
+            }
+            
+            return React.createElement(
+                config.component,
+                {
+                    key: component.id,
+                    id: component.id,
+                    name: component.name,
+                    ...config.defaultProps,
+                    ...component.props,
+                },
+                renderComponents(component.children || [])
+            )
+        })
+    }
+
+    return <div className="h-[100%]" onMouseOver={handleMouseOver}>
+        {/* <pre>
+            {JSON.stringify(components, null, 2)}
+        </pre> */}
+        {hoverComponentId}
+        {renderComponents(components)}
     </div>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App.tsx'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <DndProvider backend={HTML5Backend}>
+        <App />
+    </DndProvider>
+)
